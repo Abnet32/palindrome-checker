3c92d4a4daf8a8cf99ae37ee771babd6a6487aca
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-06-29 11:42:01 -0400

    Add dynamic color feedback for palindrome results
    Updated the palindrome checker to change the color of the result text, heading, and button based on whether the input is a palindrome. Also adjusted styles for a cleaner look and added an id to the heading for easier DOM manipulation.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const checkBtn = document.getElementById("check-btn");
 const textInput = document.getElementById("text-input");
 const resultDiv = document.getElementById("result");
-
+const head = document.getElementById("h1");
 function isPalindrome(str) {
   const cleanStr = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
   const reversed = cleanStr.split("").reverse().join("");
@@ -19,7 +19,13 @@ checkBtn.addEventListener("click", () => {
   const isPalin = isPalindrome(inputVal);
   if (isPalin) {
     resultDiv.textContent = `${inputVal} is a palindrome.`;
+    resultDiv.style.color = "#67eb34";
+    head.style.color = "#67eb34";
+    checkBtn.style.backgroundColor = "#67eb34";
   } else {
     resultDiv.textContent = `${inputVal} is not a palindrome.`;
+    resultDiv.style.color = "red";
+    head.style.color = "red";
+    checkBtn.style.backgroundColor = "red";
   }
 });
